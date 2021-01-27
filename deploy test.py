d63c2e96444b3585fb08ab91663e9f3755f034a7
pavlorubets <44904184+pavlorubets@users.noreply.github.com>
2021-01-27 11:54:38 +0200

    Add files via upload

---
+++ deploy test.py
@@ -0,0 +1,6 @@
+Python 3.8.7 (v3.8.7:6503f05dd5, Dec 21 2020, 12:45:15) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Hello, World! This is Test Deploy")
+Hello, World! This is Test Deploy
+>>> 
